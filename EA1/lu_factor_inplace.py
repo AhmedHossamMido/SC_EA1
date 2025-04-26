c14f66777e59d7b334ec899f3aad4d04957ab580
AhmedHossamMido <ahmedhossam@live.de>
2025-04-26 19:26:17 +0000

    Text- und Codeänderungen

---
+++ EA1/lu_factor_inplace.py
@@ -0,0 +1,11 @@
+
+import numpy as np
+import memray
+from scipy.linalg import lu_factor, lu
+
+n = 4096
+rng = np.random.default_rng(seed=123)
+A = rng.random((n, n))
+
+with memray.Tracker("lu_factor_inplace.bin", native_traces=True):
+    lu_factor(A, overwrite_a=True)
